c393748e36af18895cf8da4c5cfa070f2433819a
Der-Die-Das <der-die-das@hotmail.com>
2016-09-22 21:38:48 +0200

    -Added Map Scripts

---
+++ Assets/Scripts/HexMapEditor.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HexMapEditor : MonoBehaviour
+{
+
+    public Color[] colors;
+
+    public HexGrid hexGrid;
+
+
+    public GameObject placeableObject;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            HandleInput();
+        }
+    }
+
+    void HandleInput()
+    {
+        Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(inputRay, out hit))
+        {
+            hexGrid.placeObject(hit.point, placeableObject);
+        }
+    }
+
+
+}
