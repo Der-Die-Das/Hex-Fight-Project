f848dd941991bad9bc5ea4e059c8f6f367d3181e
Ofaso <kevinwuestner5@gmail.com>
2016-09-26 12:28:45 +0200

    - public Card Class -> For Attacking and Defensing Cards - public TransportCard Class -> For TransportVehicles

---
+++ Assets/Script/Classes/Card.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace HexFight
+{
+    public class Card : MonoBehaviour
+    {
+        // Member-Attributes
+        private string name;
+        private int hp;
+        private int level;
+        private int damage;
+        private int moveRange;
+        private int minAttackRange;
+        private int maxAttackRange;
+        private Type type;
+        //Hex-Coordinates
+        public enum Partition { Offense,Defense};
+        private Partition partition;
+        
+
+        public struct Type
+        {
+            private string name;
+            private Type[] strongAgainst;
+            private Type[] weakAgainst;
+
+        }
+
+        // Konstruktoren
+        public Card(string pname, int php, int plevel, int pdamage, int pmoveRange, int pminAttackRange, int pmaxAttackRange, Type ptype)
+        {
+            name = pname;
+            hp = php;
+            level = plevel;
+            damage = pdamage;
+            moveRange = pmoveRange;
+            minAttackRange = pminAttackRange;
+            maxAttackRange = pmaxAttackRange;
+            type = ptype;
+        }
+
+
+        // Member-Methoden
+    }
+}
