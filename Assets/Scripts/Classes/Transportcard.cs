e76e8078f1df7ae3ed1e3d5ed5cf6fa6d9141901
Der-Die-Das <der-die-das@hotmail.com>
2016-09-30 20:30:13 +0200

    -Merged #1_ClassStructure manually

---
+++ Assets/Scripts/Classes/Transportcard.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace HexFight
+{
+    public class Transportcard : Card
+    {
+        // Member-Attribute
+        private int capacity;
+
+        // Konstruktoren
+        public Transportcard(string pname, int php, int plevel, int pdamage, int pmoveRange, int pminAttackRange, int pmaxAttackRange, Type ptype, int pcapacity)
+            : base(pname, php, plevel, pdamage, pmoveRange, pminAttackRange, pmaxAttackRange, ptype)
+        {
+            capacity = pcapacity;
+        }
+    }
+}
