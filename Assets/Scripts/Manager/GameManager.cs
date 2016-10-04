7dd3b8bc9d0e0fc97a51924543f3b8016f1c953e
Ofaso <kevinwuestner5@gmail.com>
2016-10-04 14:08:57 +0200

    -Created Singleton GameManager -Added GameManager to DontDestroyonload -Initiate on start of Game

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    private static GameManager instance;
+
+    private GameManager()
+    {
+        if(instance == null)
+        {
+            instance = new GameManager();
+        }
+    }
+
+    void Awake()
+    {
+        instance = new GameManager();
+        DontDestroyOnLoad(instance);
+    }
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
