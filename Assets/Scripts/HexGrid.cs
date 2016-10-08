3b30baab47da079229d90729cda93437d9ef956d
Der-Die-Das <der-die-das@hotmail.com>
2016-10-08 13:44:06 +0200

    Added Hex Map Creation in Edit Mode
    -Added attributes to every hexCell so it can get placed and customised fully in Edit Mode

--- Assets/Scripts/HexGrid.cs
+++ Assets/Scripts/HexGrid.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
+[ExecuteInEditMode]
 public class HexGrid : MonoBehaviour
 {
 
@@ -19,13 +20,17 @@ public class HexGrid : MonoBehaviour
     Canvas gridCanvas;
     HexMesh hexMesh;
 
-
-    void Awake()
+    void Start()
     {
         gridCanvas = GetComponentInChildren<Canvas>();
         hexMesh = GetComponentInChildren<HexMesh>();
 
         cells = new HexCell[height * width];
+
+        foreach (var item in cells)
+        {
+            DestroyImmediate(item);
+        }
 
         for (int z = 0, i = 0; z < height; z++)
         {
@@ -34,20 +39,48 @@ public class HexGrid : MonoBehaviour
                 CreateCell(x, z, i++);
             }
         }
-    }
 
-    void Start()
-    {
         hexMesh.Triangulate(cells);
     }
 
-
-    public void placeObject(Vector3 position, GameObject objectToPlace)
+    void Update()
     {
-        position = transform.InverseTransformPoint(position);
-        HexCoordinates coordinates = HexCoordinates.FromPosition(position);
+        if (showCoordinates)
+        {
+            if (gridCanvas.GetComponentsInChildren<Transform>().Length == 1 || gridCanvas.GetComponentsInChildren<Transform>() == null)
+            {
+                foreach (var item in cells)
+                {
+                    Text label = Instantiate<Text>(cellLabelPrefab);
+                    label.rectTransform.SetParent(gridCanvas.transform, false);
+                    label.rectTransform.anchoredPosition =
+                        new Vector2(item.transform.position.x, item.transform.position.z);
+                    label.text = item.coordinates.ToStringOnSeparateLines();
+                }
+            }
+        }
+        else
+        {
+            Transform[] labels = gridCanvas.GetComponentsInChildren<Transform>();
+
+            for (int i = 0; i < labels.Length; i++)
+            {
+                if (labels[i].name != "Canvas")
+                {
+                    DestroyImmediate(labels[i].gameObject);
+                }
+            }
+        }
+    }
+
+
+    public GameObject placeObject(HexCoordinates coordinates, GameObject objectToPlace)
+    {
+        //position = transform.InverseTransformPoint(position);
+        //HexCoordinates coordinates = HexCoordinates.FromPosition(position);
         int index = coordinates.X + coordinates.Z * width + coordinates.Z / 2;
         HexCell cell = cells[index];
+        GameObject placedObject;
         if (objectToPlace != null)
         {
             if ((cell.placedObject != null && cell.placedObject.name != objectToPlace.name) || cell.placedObject == null)
@@ -57,17 +90,20 @@ public class HexGrid : MonoBehaviour
                     Destroy(cell.placedObject);
                     cell.placedObject = null;
                 }
-                    GameObject placedObject = Instantiate(objectToPlace);
-                    cell.placedObject = placedObject;
-                    placedObject.transform.position = cell.transform.position;
-                    placedObject.transform.position = new Vector3(placedObject.transform.position.x, placedObject.transform.localScale.y / 2, placedObject.transform.position.z);
-                    placedObject.transform.SetParent(objectParent.transform, true);
+                placedObject = Instantiate(objectToPlace);
+                cell.placedObject = placedObject;
+                placedObject.transform.position = cell.transform.position;
+                placedObject.transform.position = new Vector3(placedObject.transform.position.x, placedObject.transform.localScale.y / 2, placedObject.transform.position.z);
+                placedObject.transform.SetParent(objectParent.transform, true);
+                return placedObject;
             }
-        }else
-        {
-                Destroy(cell.placedObject);
-                cell.placedObject = null;
         }
+        else
+        {
+            Destroy(cell.placedObject);
+            cell.placedObject = null;
+        }
+        return null;
     }
 
     void CreateCell(int x, int z, int i)
@@ -82,14 +118,5 @@ public class HexGrid : MonoBehaviour
         cell.transform.localPosition = position;
         cell.coordinates = HexCoordinates.FromOffsetCoordinates(x, z);
         //cell.color = Color.white;
-
-        if (showCoordinates)
-        {
-            Text label = Instantiate<Text>(cellLabelPrefab);
-            label.rectTransform.SetParent(gridCanvas.transform, false);
-            label.rectTransform.anchoredPosition =
-                new Vector2(position.x, position.z);
-            label.text = cell.coordinates.ToStringOnSeparateLines();
-        }
     }
 }
