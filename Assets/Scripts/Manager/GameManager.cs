378f202964046815bf426f4ff1ea2bfbb8aefb51
Der-Die-Das <der-die-das@hotmail.com>
2016-11-15 08:33:41 +0100

    - ReCreated the HexMap Design

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,45 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameManager : MonoBehaviour {
-
-    private static GameManager instance;
-
-    private GameManager()
+namespace HexFight
+{
+    public class GameManager : MonoBehaviour
     {
-        if(instance == null)
+
+        private static GameManager instance;
+
+        private GameManager()
         {
-            instance = this;
+            if (instance == null)
+            {
+                instance = this;
+            }
         }
-    }
 
-    void Awake()
-    {
-        
-    }
+        void Awake()
+        {
+
+        }
 
 
-	// Use this for initialization
-	void Start () {
-        DontDestroyOnLoad(instance);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        // Use this for initialization
+        void Start()
+        {
+            DontDestroyOnLoad(instance);
+        }
 
-    public static GameManager GetInstance() {
-        return instance;
-    }
+        // Update is called once per frame
+        void Update()
+        {
+           
+        }
 
-    public void LoadLevelSelection() {
-        //Load LevelSelection
-        //Application.LoadLevel();
-    }
+        public static GameManager GetInstance()
+        {
+            return instance;
+        }
 
-    public void LoadDeckManager() {
-        //Load DeckManager
-        //Application.LoadLevel();
+        public void LoadLevelSelection()
+        {
+            //Load LevelSelection
+            //Application.LoadLevel();
+        }
+
+        public void LoadDeckManager()
+        {
+            //Load DeckManager
+            //Application.LoadLevel();
+        }
     }
 }
